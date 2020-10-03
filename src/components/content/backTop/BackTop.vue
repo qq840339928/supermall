f10571ad984b3a865109a57f99287d785ac26630
gy <1234@qq.com>
2020-10-03 20:12:44 +0800

    betterScrol

--- src/components/content/backTop/BackTop.vue
+++ src/components/content/backTop/BackTop.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="back_top">
+  <div class="back_top" @click="backToTop">
     <img src="~assets/img/common/top.png" alt="" />
   </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    backToTop () {
+      this.$emit('backToTop')
+    }
+  },
+};
 </script>
 <style scoped>
 .back_top {
   position: fixed;
-  right: 0.64rem;
-  bottom: 2.13rem;
+  top: 486px;
+  right: 8px;
+  z-index: 999;
 }
-.back_top img {
-  width: 1.83rem;
-  height: 1.83rem;
+.back_top > img {
+  width: 42px;
+  height: 42px;
 }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,120 +5,22 @@
         购物街
       </template>
     </NavBar>
-    <!-- 轮播图 -->
-    <HomeSwiper :sun_banners = 'banners' class="home_HomeSwiper"/>
-    <!--推荐recommend部分就几个图片 -->
-    <HomeRecommends :sun_recommends = 'recommend'/>
-    <hr class="HomeFeatureTopHr"/>
-    <!--流行-->
-    <HomeFeature/>
-    <!-- 流行-新款-精选 -->
-    <TabControl :titles="['流行', '新款', '精选']" @setCurrentIndex="setCurrentIndex"/>
-    <!-- 详细数据展示 -->
-    <goodsList :goodsList="goods[currentType].data"></goodsList>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>11</li>
-      <li>12</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>18</li>
-      <li>19</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>25</li>
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-      <li>30</li>
-      <li>31</li>
-      <li>32</li>
-      <li>33</li>
-      <li>34</li>
-      <li>35</li>
-      <li>36</li>
-      <li>37</li>
-      <li>38</li>
-      <li>39</li>
-      <li>40</li>
-      <li>41</li>
-      <li>42</li>
-      <li>43</li>
-      <li>44</li>
-      <li>45</li>
-      <li>46</li>
-      <li>47</li>
-      <li>48</li>
-      <li>49</li>
-      <li>50</li>
-      <li>51</li>
-      <li>52</li>
-      <li>53</li>
-      <li>54</li>
-      <li>55</li>
-      <li>56</li>
-      <li>57</li>
-      <li>58</li>
-      <li>59</li>
-      <li>60</li>
-      <li>61</li>
-      <li>62</li>
-      <li>63</li>
-      <li>64</li>
-      <li>65</li>
-      <li>66</li>
-      <li>67</li>
-      <li>68</li>
-      <li>69</li>
-      <li>70</li>
-      <li>71</li>
-      <li>72</li>
-      <li>73</li>
-      <li>74</li>
-      <li>75</li>
-      <li>76</li>
-      <li>77</li>
-      <li>78</li>
-      <li>79</li>
-      <li>80</li>
-      <li>81</li>
-      <li>82</li>
-      <li>83</li>
-      <li>84</li>
-      <li>85</li>
-      <li>86</li>
-      <li>87</li>
-      <li>88</li>
-      <li>89</li>
-      <li>90</li>
-      <li>91</li>
-      <li>92</li>
-      <li>93</li>
-      <li>94</li>
-      <li>95</li>
-      <li>96</li>
-      <li>97</li>
-      <li>98</li>
-      <li>99</li>
-      <li>100</li>
-    </ul>
-
+    <Scroll class="wrapper" ref="scroll">
+      <!-- 轮播图 -->
+      <HomeSwiper :sun_banners = 'banners' class="home_HomeSwiper"/>
+      <!--推荐recommend部分就几个图片 -->
+      <HomeRecommends :sun_recommends = 'recommend'/>
+      <hr class="HomeFeatureTopHr"/>
+      <!--流行-->
+      <HomeFeature/>
+      <!-- 流行-新款-精选 -->
+      <TabControl :titles="['流行', '新款', '精选']" @setCurrentIndex="setCurrentIndex"/>
+      <!-- 详细数据展示 -->
+      <goodsList :goodsList="goods[currentType].data"></goodsList>
+    </Scroll>
+    <back-top 
+      @backToTop = 'backToTop'
+    />
   </div>
 </template>
 
@@ -130,6 +32,8 @@ import HomeFeature from './childComps/HomeFeature'
 
 import TabControl from 'components/content/tabControl/TabControl'
 import GoodsList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
 import { getHomeMultiData, getHomeGoods } from 'network/home.js'
 export default {
@@ -140,7 +44,9 @@ export default {
     HomeRecommends,
     HomeFeature,
     TabControl,
-    GoodsList
+    GoodsList,
+    Scroll,
+    BackTop
   },
   data () {
     return{
@@ -193,6 +99,12 @@ export default {
         this.goods[type].data = res.data.list
         this.goods[type].page = ++page
       })
+    },
+    /**
+     * 右下角返回顶部按钮
+     */
+    backToTop () {
+      this.$refs.scroll.scrollToTop()
     }
   }
 }
@@ -217,5 +129,14 @@ export default {
   border: 0;
   background-color:darkgray;
   height: 15px;
+}
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0px;
+  bottom: 13.44px;
+  overflow: hidden;
+  background-color: #fff;
 }
 </style>
