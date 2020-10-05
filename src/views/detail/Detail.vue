3b274e74b769f6d141d080cdb4ce69ef79778ae7
gy <1234@qq.com>
2020-10-05 14:01:41 +0800

    详情页头

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="detail">
-    详情页
+    <NavBar>
+      <template v-slot:left>
+        <div class="back" @click="back">
+          <img src="~assets/img/common/back.svg" alt="">
+        </div>
+      </template>
+      <template v-slot:center>
+        <div class="titles">
+          <div v-for="(item, index) in titles" :key='index' :class="{active: index === currentIndex}" @click="itemClick(index)">
+            <span>{{item}}</span>
+          </div>
+        </div>
+      </template>
+    </NavBar>
   </div>
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
   name: "detail",
   components: {
+    NavBar
   },
   data() {
     return {
+      titles: ["商品", "参数", "评论", "推荐"],
+      currentIndex: 0
     };
   },
   created() {
-  }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    },
+    /**
+     * 导航栏的返回键
+     */
+    back() {
+      this.$router.back()
+    }
+  },
 }
 </script>
 <style scoped>
-
-</style>>
-  
+.titles{
+  display: flex;
+  justify-content: space-evenly;
+}
+.active{
+  color: var(--color-tint);
+}
+.back > img{
+  margin-top: 10px;
+}
 </style>
+  
